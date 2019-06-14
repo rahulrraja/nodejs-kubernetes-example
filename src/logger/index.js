90be46053c1d8ed5598e17583e131de83cafaa00
Rahul Raja <Rahul.Raja@ril.com>
2019-06-14 12:08:18 +0530

    Committing changes

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,7 +1,8 @@
 const winston = require('winston')
+const logLevel = process.env.logLevel
 
 const logger = winston.createLogger({
-    level: process.env.logConfiguration ? process.env.logConfiguration['level'] : 'info',
+    level: logLevel ? logLevel : 'info',
     format: winston.format.simple(),
     transports: [
         new winston.transports.Console()
